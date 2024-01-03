d7cbcbf706ed4b51c471c76de79b538123196724
Ulugbek Abdullaev <ulugbekna@gmail.com>
2024-01-03 20:08:14 +0500

    print field names when printing the AST

--- src/controller.ts
+++ src/controller.ts
@@ -92,7 +92,7 @@ export function createNotebookController(extensionUri: vscode.Uri) {
 					}
 
 				} else {
-					data = printParseTree(parseTree.rootNode, 0).join('\n');
+					data = printParseTree(parseTree.rootNode).join('\n');
 				}
 
 				await updateOutput(execution, data);
@@ -110,10 +110,42 @@ export function createNotebookController(extensionUri: vscode.Uri) {
 	});
 }
 
-function printParseTree(node: Parser.SyntaxNode, depth = 0): string[] {
-	const data = [' '.repeat(depth * 2) + node.type + ` [${node.startPosition.row}, ${node.startPosition.column}] - [${node.endPosition.row}, ${node.endPosition.column}]`];
-	for (const child of node.namedChildren) {
-		data.push(...printParseTree(child, depth + 1));
+function printParseTree(node: Parser.SyntaxNode): string[] {
+	const printedNodes: string[] = [];
+
+	const cursor = node.walk();
+	let depth = 0;
+	let lastSeenDepth = 0;
+
+	// depth-first pre-order tree traversal
+	while (depth >= 0) {
+		const isNodeUnexplored = lastSeenDepth <= depth;
+
+		if (isNodeUnexplored && cursor.currentNode().isNamed()) {
+			const currentNode = cursor.currentNode();
+			printedNodes.push(printNode(currentNode, depth, cursor.currentFieldName()));
+		}
+
+		lastSeenDepth = depth;
+
+		if (isNodeUnexplored && cursor.gotoFirstChild()) {
+			++depth;
+			continue;
+		}
+
+		if (cursor.gotoNextSibling()) {
+			continue;
+		}
+
+		cursor.gotoParent();
+		--depth;
 	}
-	return data;
+
+	return printedNodes;
 }
+
+function printNode(node: Parser.SyntaxNode, depth: number, fieldName: string | undefined) {
+	const indent = ' '.repeat(depth * 2);
+	const fieldNameStr = fieldName ? `${fieldName}: ` : '';
+	return `${indent}${fieldNameStr}${node.type} [${node.startPosition.row}, ${node.startPosition.column}] - [${node.endPosition.row}, ${node.endPosition.column}]`;
+}	
